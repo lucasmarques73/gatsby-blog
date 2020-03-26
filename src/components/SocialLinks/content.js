b9be32fe0922d356f58ba7e8d15a31c0c7c2bc16
Lucas Marques <lucasmarques73@hotmail.com>
2020-03-25 23:28:02 -0300

    SocialLinks Component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/lucasmarques73",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/lucas_marques",
+  },
+  {
+    label: "Linkedin",
+    url: "https://linkedin.com/in/lucasmarques73",
+  },
+]
+
+export default links
