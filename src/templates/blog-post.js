cef7eb0e53bcbc406326aabb4b56e7255b80f182
Lucas Marques <lucasmarques73@hotmail.com>
2020-04-21 18:19:59 -0300

    Work with images in gatsby

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,7 +16,7 @@ const BlogPost = ({ data, pageContext }) => {
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description}
-        image={post.frontmatter.image}
+        // image={post.frontmatter.image}
       />
       <S.PostHeader>
         <S.PostDate>
@@ -42,7 +42,7 @@ export const query = graphql`
         title
         description
         date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-        image
+        # image
       }
       html
       timeToRead
