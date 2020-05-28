2d92538f6ba21ba07804848f274b0e285e4b5a52
Lucas Marques <lucasmarques73@hotmail.com>
2020-05-28 20:56:00 -0300

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,40 @@
 import React from "react"
+import styled from "styled-components"
+import media from "styled-media-query"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Não encontrado" />
-    <h1>ops..</h1>
-    <p>Deu ruim..</p>
-  </Layout>
-)
+import * as S from "../components/Post/styled"
+
+const NotFoundPage = ({ data }) => {
+  const html = data.markdownRemark.html
+  const page = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <SEO title={page.title} image={page.image} />
+      <S.PostHeader>
+        <S.PostTitle>{page.title}</S.PostTitle>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      </S.MainContent>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query NotFound {
+    markdownRemark(frontmatter: { category: { eq: "notFound" } }) {
+      frontmatter {
+        title
+        image
+      }
+      html
+    }
+  }
+`
 
 export default NotFoundPage
